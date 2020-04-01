b6d17723ba72405e1e34b0b00fa4b364ea58ce29
szenadam <szenadam1@gmail.com>
2020-04-01 15:55:00 +0200

    Add pug template engine, use it to render index page

--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.set("view engine", "pug");
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
