baa26da4d79d01f9ffb48f2cd08c1b489ad283cd
szenadam <szenadam1@gmail.com>
2020-04-02 18:01:00 +0200

    Add bodyParser

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const bodyParser = require("body-parser");
 
+// Configure application
 app.set("view engine", "pug");
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
   res.render("index");
