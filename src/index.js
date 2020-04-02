6a54fec09369c58534b4db0bfd0b4e66fe72dede
szenadam <szenadam1@gmail.com>
2020-04-02 18:10:00 +0200

    Add cookieParser

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 
 // Configure application
 app.set("view engine", "pug");
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.render("index");
