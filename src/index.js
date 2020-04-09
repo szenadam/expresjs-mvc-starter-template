2d14ab316563d19ab39cdf6aec22178f30224e9b
szenadam <szenadam1@gmail.com>
2020-04-09 18:17:00 +0200

    Fix lint errors in index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const dotenvFile = require("dotenv").config();
+const dotenvFile = require('dotenv').config();
 if (dotenvFile.error) throw dotenvFile.error;
 const port = process.env.PORT;
-const bodyParser = require("body-parser");
-const cookieParser = require("cookie-parser");
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
 // Configure application
-app.set("view engine", "pug");
+app.set('view engine', 'pug');
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-app.get("/", (req, res) => {
-  res.render("index");
+app.get('/', (req, res) => {
+  res.render('index');
 });
 
 app.listen(port, () =>
