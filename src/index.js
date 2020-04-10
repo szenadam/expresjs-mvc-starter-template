d1ddb2c50318edf06bad23138e4f12fe022ea0d4
szenadam <szenadam1@gmail.com>
2020-04-10 19:34:00 +0200

    Create HomeController, move logic there

--- src/index.js
+++ src/index.js
@@ -6,14 +6,15 @@ const port = process.env.PORT;
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 
+// Load Controllers
+const homeController = require('./controllers/HomeController');
+
 // Configure application
 app.set('view engine', 'pug');
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-app.get('/', (req, res) => {
-  res.render('index');
-});
+app.get('/', homeController);
 
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
